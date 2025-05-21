464f81da03eec96000a7e8be7af9450b3671fc5d
ines silva <ines27matos09silva@gmail.com>
2025-05-21 09:39:23 +0000

    [FIX] change url redirect

--- toc_invoice/utils.py
+++ toc_invoice/utils.py
@@ -1,5 +1,5 @@
 TOC_BASE_URL = 'https://api9.toconline.pt'
-redirect_uri = 'https://0699-2001-818-de5d-da00-c214-7e3d-a47b-193b.ngrok-free.app/oauth/callback'
+redirect_uri = 'https://oauth.pstmn.io/v1/callback'
 
 auth_url = 'https://app9.toconline.pt/oauth'
 token_url = 'https://app9.toconline.pt/oauth/token'
